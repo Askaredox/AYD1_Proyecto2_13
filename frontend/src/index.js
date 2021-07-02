17d9fb0cf7b6ba0a0b9d570dd17107e142ea0d8e
Ozmar Escobar <25725331+ZantetsukenGT@users.noreply.github.com>
2021-07-01 22:13:08 -0600

    pm2 graceful start y stop

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,3 +16,11 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+process.on('SIGINT', () =>
+{
+  process.exit(0)
+})
+
+process.send('ready')
+
